082dad29e220c986e4f89fa174b3f95e1b039255
Otman404 <outhmaneboughaba98@gmail.com>
2018-11-27 00:00:36 +0100

    .

--- TP3/Ex2/Nombres.java
+++ TP3/Ex2/Nombres.java
@@ -1,15 +1,29 @@
 class Nombres{
 
-	private int[] Tab;
+	private int[] tab;
 	private int n; // array length
 
-	public Nombres(){}
-	public Nombres(int[] Tab,int n){this.Tab = Tab; this.n = n;}
+	public Nombres(){
+		this.n = 1;
+		this.tab = new int[n];
+	}
+	public Nombres(int[] tab,int n){
+		this.n = n;
+		this.tab = new int[n];
+		for (int i = 0;i<n ;i++ ) {
+			this.tab[i] = tab[i]; 
+		}
+	}
 
-	public int[] getTab(){return this.Tab;}
-	public void setTab(int[] Tab){this.Tab = Tab;}
+	public int[] getTab(){return this.tab;}
+	public void setTab(int[] tab){
+		this.tab = new int[tab.length];
+		for (int i = 0;i<n ;i++ ) {
+			this.tab[i] = tab[i]; 
+		}
+	}
 
-	public int getLength(){return n;}
+	public int getLength(){return this.n;}
 	public void setLength(int n){
 		if(n <= 0)
 			n = 1;
@@ -19,7 +33,7 @@ class Nombres{
 
 	public int getSum(){
 		int s = 0;
-		for (int n : Tab) {
+		for (int n : tab) {
 			s+=n;
 		}
 		return s;
@@ -27,7 +41,7 @@ class Nombres{
 
 	public int getProd(){
 		int p = 1;
-		for (int n : Tab ) {
+		for (int n : tab ) {
 			p*=n;
 		}
 		return p;
@@ -35,15 +49,15 @@ class Nombres{
 
 	public int getDiff(){
 		int d = 0;
-		for (int n : Tab ) {
+		for (int n : tab ) {
 			d-=n;
 		}
 		return d;
 	}
 
 	public int getMax(){
-		int max = this.Tab[0];
-		for (int n : Tab ) {
+		int max = this.tab[0];
+		for (int n : tab ) {
 			if(n>max)
 				max = n;
 		}
@@ -51,8 +65,8 @@ class Nombres{
 	}
 
 	public int getMin(){
-		int min = this.Tab[0];
-		for (int n : Tab ) {
+		int min = this.tab[0];
+		for (int n : tab ) {
 			if(n<min)
 				min = n;
 		}
@@ -61,9 +75,9 @@ class Nombres{
 
 	public void ShowMultTable(){
 		for (int i = 0 ;i<n ;i++ ) {
-			System.out.println("Table de multiplication Pour le nombre :"+this.Tab[i]);
+			System.out.println("table de multiplication Pour le nombre :"+this.tab[i]);
 			for (int j = 1 ;j<10 ;j++ ) {
-				System.out.println(this.Tab[i]+" * "+j+" = "+(this.Tab[i]*j));
+				System.out.println(this.tab[i]+" * "+j+" = "+(this.tab[i]*j));
 			}
 			System.out.println("\n\n");
 		}
@@ -79,21 +93,21 @@ class Nombres{
 	public void getFactOfEveryElement(){
 		System.out.println("Factoriel de chaque element du tableau : \n");
 		for (int i = 0 ;i<n ;i++ ) {
-			System.out.println(this.Tab[i]+"! = "+getFact(this.Tab[i]));
+			System.out.println(this.tab[i]+"! = "+getFact(this.tab[i]));
 		}
 	}
 // Autre methode pour calculer factoriel des elements du tableau
 	public void FACTT(){
 		for (int i = 0 ;i<this.n ;i++ ) {
-			if((this.Tab[i] == 1) || (this.Tab[i] == 0)){
-				System.out.println(this.Tab[i]+"! = 1");
+			if((this.tab[i] == 1) || (this.tab[i] == 0)){
+				System.out.println(this.tab[i]+"! = 1");
 			}
 			else{
-					int r = this.Tab[i];
-				for (int j = (this.Tab[i]-1);j>1 ;j-- ) {
+					int r = this.tab[i];
+				for (int j = (this.tab[i]-1);j>1 ;j-- ) {
 					r = r * j;
 				}
-				System.out.println(this.Tab[i]+"! = "+r);
+				System.out.println(this.tab[i]+"! = "+r);
 			}
 		}
 	}
